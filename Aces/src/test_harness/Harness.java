fbef1dbe8aaf8999a73e6df019ef0479b7a2b9b5
Tristan <s3528615@student.rmit.edu.au>
2016-06-29 21:00:31 +1000

    retry update

--- Aces/src/test_harness/Harness.java
+++ Aces/src/test_harness/Harness.java
@@ -1,15 +1,10 @@
 package test_harness;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 import model.card.Card;
-import model.card.Face;
 import model.card.TopHand;
-import model.card.Tup;
-import model.card.WinningHands;
+import model.facade.AcesModel;
 import model.game.Game;
 import model.game.texas.TexasPoker;
 import model.player.ComputerPlayer;
@@ -17,59 +12,37 @@ import model.player.HumanPlayer;
 import model.player.Player;
 import model.table.Table;
 import model.table.TexasTable;
+import view.main.MainView;
 
 public class Harness
 {
+    private static AcesModel acesFacade;
 
     public Harness()
     {
         // TODO Auto-generated constructor stub
     }
-    
-    public static void dealTest(Table table)
-    {
-        System.out.println("preparing Players");
-        table.addPlayer(new ComputerPlayer("Taylor"));
-        table.addPlayer(new ComputerPlayer("Mathew"));
-        table.addPlayer(new HumanPlayer("Tristan"));
-        table.addPlayer(new HumanPlayer("Manuel"));
-        System.out.println("Players added");
-        table.dealCards();
-        System.out.println("cards dealt");
-        Card[] flop = table.getCardsInPlay();
-        Player[] players = table.getSeats();
-        for (int i = 0; i < players.length; ++i)
-        {
-            if(players[i] != null)
-            {    
-                System.out.printf("%s\n",players[i].toString());
-            }
-        }
-        System.out.println("The shared cards are: \n");
-        for (Card c : flop)
-        {
-            System.out.printf("%s\n", c.toString());
-        }
-    }
+
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
         Game poker = new TexasPoker();
-        Table table; 
+        Table table;
         Scanner in = new Scanner(System.in);
         int result = -1;
         do
         {
             System.out.printf("-----------------------------\n");
-            System.out.printf("%10s\n", "Tests Harness" );
+            System.out.printf("%10s\n", "Tests Harness");
             System.out.printf("-----------------------------\n");
             System.out.printf("%-10d%s\n", 1, "Deal cards");
             System.out.printf("%-10d%s\n", 2, "Check win conditions");
+            System.out.printf("%-10d%s\n", 3, "Test the main view");
             System.out.printf("%-10d%s\n", 0, "Exit");
             System.out.printf("\n%s:", "Enter selection");
             result = in.nextInt();
-            
-            switch(result)
+
+            switch (result)
             {
                 case 1:
                 {
@@ -87,9 +60,9 @@ public class Harness
                     Player[] players = table.getSeats();
                     for (int i = 0; i < players.length; ++i)
                     {
-                        if(players[i] != null)
-                        {    
-                            System.out.printf("%s\n",players[i].toString());
+                        if (players[i] != null)
+                        {
+                            System.out.printf("%s\n", players[i].toString());
                         }
                     }
                     System.out.println("The shared cards are: \n");
@@ -105,43 +78,16 @@ public class Harness
                     // If there is no players add new
                     table = new TexasTable();
                     poker.addTable(table);
-//                    if (table.getSeats().length < 1)
-//                    {
-                        table.addPlayer(new ComputerPlayer("Taylor"));
-                        table.addPlayer(new ComputerPlayer("Mathew"));
-                        table.addPlayer(new HumanPlayer("Tristan"));
-                        table.addPlayer(new HumanPlayer("Manuel"));
-//                    }
-                    // Deal hand
+
+                    table.addPlayer(new ComputerPlayer("Taylor"));
+                    table.addPlayer(new ComputerPlayer("Mathew"));
+                    table.addPlayer(new HumanPlayer("Tristan"));
+                    table.addPlayer(new HumanPlayer("Manuel"));
+
                     table.dealCards();
-//                    List<Card> cards = new ArrayList<Card>(Arrays.asList(
-//                                                    table.getCardsInPlay()));
-//                    Card[] flop = table.getCardsInPlay();
-//                    Player[] players = table.getSeats();
-//                    for (int i = 0; i < players.length; ++i)
-//                    {
-//                        if(players[i] != null)
-//                        {    
-//                            cards.add(players[i].getHand()[0]);
-//                            cards.add(players[i].getHand()[1]);
-//                            List<Tup<WinningHands, Face>> bestHand = 
-//                                    th.processHand(cards.toArray(new Card[7]));
-//                            System.out.printf("%s\n",players[i].toString());
-////                            System.out.printf("%s high \n", bestHand.get(0).l,
-////                                    bestHand.get(0).f);
-//                            cards.clear();
-//                            cards = new ArrayList<Card>(Arrays.asList(
-//                                    table.getCardsInPlay()));
-//                            
-//                            
-//                        }
-//                    }
+
                     Player[] winner = null;
-//                    System.out.println("The shared cards are: \n");
-//                    for (Card c : flop)
-//                    {
-//                        System.out.printf("%s\n", c.toString());
-//                    }
+
                     System.out.println("The winner('s) are: ");
                     try
                     {
@@ -152,30 +98,39 @@ public class Harness
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                    if(winner != null && winner.length > 0)
+                    if (winner != null && winner.length > 0)
                     {
-                        for ( Player p : winner)
+                        for (Player p : winner)
                         {
                             if (p != null)
                             {
-                                System.out.printf("%s high %s\n", p.getBestHand().l, p.getBestHand().f);
-                        
+                                System.out.printf("%s high %s\n",
+                                        p.getBestHand().l, p.getBestHand().f);
+
                             }
                         }
                     }
-                    
+                    break;
+
                 }
+                    
+                case 3:
+                    // setup facade
+                    Harness.acesFacade = new AcesModel();
+                    // setup main view
+                    MainView mainView = new MainView(Harness.acesFacade);
+                    mainView.setVisible(true);
+                    break;
                 case 0:
                     break;
                 default:
-                    System.out.println("Enter the number corresponding" +
-                                        "to you selection");
+                    System.out.println("Enter the number corresponding"
+                            + "to you selection");
                     break;
-                    
+
             }
-        }while(result != 0);
+        } while (result != 0);
 
     }
-    
 
 }
